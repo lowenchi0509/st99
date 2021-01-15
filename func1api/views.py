bac24a76951a8178b1c19052a6e530c09760d79c
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2021-01-15 23:56:59 +0800

    Update views.py

--- func1api/views.py
+++ func1api/views.py
@@ -59,15 +59,15 @@ def callback(request):
               
                    elif mtext == '@按鈕樣板':
                         func.sendButton(event)
-                   elif mtext == '@旅遊數據分析':
+                   elif mtext == '旅遊數據分析':
                         func.sendButtonp(event)
-                   elif mtext == '@景點查詢':
+                   elif mtext == '景點查詢':
                         func.sendButtonq(event)
-                   elif mtext == '@交通一點通':
+                   elif mtext == '交通一點通':
                         func.sendButtonr(event)
-                   elif mtext == '@旅遊即時報':
+                   elif mtext == '旅遊即時報':
                         func.sendButtons(event)
-                   elif mtext == '@天氣小幫手':
+                   elif mtext == '天氣小幫手':
                         func.sendButtont(event)
                    elif mtext == '@確認樣板':
                         func.sendConfirm(event)
